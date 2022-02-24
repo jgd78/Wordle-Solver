1a15ce30f4e0602e21f6eb886160fedf7ec7c78d
James DImperio <jamesdimperio@hotmail.com>
2022-02-24 02:24:35 -0500

    Delete testCode.py

--- testCode.py
+++
@@ -1,6 +0,0 @@
-import ast
-with open('wordleWords.txt') as file:
-    possanswers=ast.literal_eval(file.readline())
-f=open("wordleWordsSorted.txt", "w")
-f.write(str(sorted(possanswers)))
-f.close()
